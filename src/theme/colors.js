f1cc2c3ebf329c6307cac8e9359da5e1f4636c98
isaac <isaac@vizir.com.br>
2020-09-22 05:02:06 -0300

    feat(theme): add colors theme

---
+++ src/theme/colors.js
@@ -0,0 +1,22 @@
+export default {
+  primaryColor: "#ffd600",
+  primaryLightColor: "#ffff52",
+  primaryDarkColor: "#c7a500",
+  primaryTextColor: "#000000",
+  secondaryColor: "#157db8",
+  secondaryLightColor: "#5caceb",
+  secondaryDarkColor: "#005188",
+  secondaryTextColor: "#ffffff",
+
+  shadeLight: "#f4f1e7",
+  shadeMedium: "#ebe6d9",
+  shadeDark: "#d6d1c6",
+
+  textDark: "#2c2c2c",
+  textClear: "#fffcf4",
+
+  successColor: "#75bc4a",
+  dangerColor: "#D43E3E",
+
+  dinamicColor: "rgba(0,0,0,0.4)",
+};
