e19df56f427e4fab84e2b78cee19dd877a9cdc60
isaac <isaac@vizir.com.br>
2020-09-23 06:18:39 -0300

    feat(styled): add custom field component

---
+++ src/Components/Styled/Custom.style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+import { Field } from "formik";
+import colors from "../../theme/colors";
+
+export const Search = styled(Field)`
+  height: 40px;
+  padding: 0 30px;
+  color: ${colors.textDark};
+  font-size: 12px;
+  font-weight: bold;
+  background-color: ${colors.shadeMedium};
+  border: none;
+  border: solid 2px ${colors.primaryDarkColor};
+  border-radius: 20px;
+
+  &:focus {
+    outline: none;
+    border: solid 2px ${colors.primaryLightColor};
+  }
+
+  &::placeholder {
+    font-weight: normal;
+    color: ${colors.dinamic};
+  }
+
+  ${({ block }) =>
+    block &&
+    `
+    width: 100%;
+  `}
+
+  ${({ width }) =>
+    width &&
+    `
+    width: ${width};
+  `}
+`;
