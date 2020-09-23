797bcf17c8e9461b83b55b6b8c455d5977d43289
isaac <isaac@vizir.com.br>
2020-09-23 06:33:38 -0300

    feat(app): add route for new pages

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/Home/Home";
 import HistoryPage from "./pages/History/History";
 import RankingPage from "./pages/Ranking/Ranking";
+import DetailsPage from "./pages/Details/Details";
+import SearchPage from "./pages/Search/Search";
 
 function App() {
   return (
@@ -14,6 +16,12 @@ function App() {
           </Route>
           <Route path="/history">
             <HistoryPage />
+          </Route>
+          <Route path="/details">
+            <DetailsPage />
+          </Route>
+          <Route path="/search">
+            <SearchPage />
           </Route>
           <Route path="/">
             <HomePage />
