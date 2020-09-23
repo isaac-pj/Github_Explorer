3bdc60a37ef4d29828bcceae80667439a0fef53c
isaac <isaac@vizir.com.br>
2020-09-23 05:46:29 -0300

    feat(pages): add details page

---
+++ src/pages/Details/Details.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import colors from "../../theme/colors";
+
+import PageContainer from "../../Components/Composed/PageContainer";
+
+export const DetailsPage = (props) => {
+  return <PageContainer></PageContainer>;
+};
+
+export default DetailsPage;
