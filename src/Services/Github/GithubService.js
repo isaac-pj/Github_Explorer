9098560fee060f348216b62a270878881aace2c2
isaac <isaac@vizir.com.br>
2020-09-24 01:50:25 -0300

    feat(github): add auth by personal token
    this is necessary to increase the request rate limit for the github api

--- src/Services/Github/GithubService.js
+++ src/Services/Github/GithubService.js
@@ -1,26 +1,25 @@
 import axios from "axios";
 
-// const client_id = "Iv1.ca6c3c58ccdfa06b";
-// const client_secret = "eaaf222d5c4d064b634159bfdf9398cb42d35519";
+const client_id = "Iv1.ca6c3c58ccdfa06b";
+const client_secret = "eaaf222d5c4d064b634159bfdf9398cb42d35519";
+const personal_token = "ba12995bf799f95689a902e9adb8b9dbcbb21bbd";
 
 const api = axios.create({
   baseURL: "https://api.github.com",
-  params: {
-    client_id: "Iv1.ca6c3c58ccdfa06b",
-    client_secret: "eaaf222d5c4d064b634159bfdf9398cb42d35519",
-  },
+  params: {},
+  headers: { Authorization: `Bearer ${personal_token}` },
 });
 
+const config = {
+  headers: { Authorization: `Bearer ${personal_token}` },
+};
+
 export const searchUser = async (string) => {
-  const response = await api.get(
-    `/search/users?q=${string}&page=0&per_page=15`
-  );
+  const response = await api.get(`/search/users?q=${string}&page=0&per_page=3`);
   return response.data;
 };
 
 export const request = async (url) => {
-  const response = await axios.get(
-    `${url}?client_id=${api.params.client_id}&client_secret=${api.params.client_secret}`
-  );
+  const response = await axios.get(`${url}`, config);
   return response.data;
 };
